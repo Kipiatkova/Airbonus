e6740f85ae5e90591eb3867ece3546d951349622
Kipiatkova <kipiatkova1998@mail.ru>
2021-12-01 22:36:24 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 300;
+        int miles =  20;
+        int bonus = ticket / miles;
+        System.out.println(bonus);
+    }
+}
